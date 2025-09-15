96bea8f7460854050602c9e33506a2da685a0dd8
nathaliagorito-fjg <nathaliagorito.fjg@gmail.com>
2025-09-15 14:15:10 -0300

    Add files via upload

---
+++ Interface.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import Planilhas
+from pandastable import Table
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+
+def carregaPlanilhas(tipo):
+    diretorio = askopenfilename(filetypes=[('Excel files', '*.xlsx *.xls')])
+
+    if not diretorio:
+        return
+    
+    if tipo == 'mensal':
+        if 'mensal' not in diretorio.lower():
+            labelMensagem['text'] = 'Planilha errada.'
+        else:
+            Planilhas.planilhaMensal = pd.read_excel(diretorio)
+
+            labelMensagem['text'] = 'Planilha Mensal carregada.'
+    elif tipo == 'minibio':
+        if 'minibio' not in diretorio.lower():
+            labelMensagem['text'] = 'Planilha errada.'
+        else:
+            Planilhas.planilhaMinibio = pd.read_excel(diretorio)
+
+            labelMensagem['text'] = 'Planilha Minibio carregada.'
+
+def mostraPlanilha(planilha, titulo):
+    if planilha is None or planilha.empty:
+        labelMensagem['text'] = 'Nada para mostrar!'
+    else:
+        novaJanela = Toplevel(janela)
+        novaJanela.title(titulo)
+        novaJanela.geometry('1300x500')
+
+        framePlanilha = Frame(novaJanela)
+        framePlanilha.pack(fill=BOTH, expand=1)
+
+        tabela = Table(framePlanilha, dataframe=planilha)
+        tabela.show()
+
+def processaPlanilhas():
+    nomesDuplicados, datasDuplicadas, planilhasMescladas = Planilhas.processaPlanilhas()
+    if nomesDuplicados is None:
+        labelMensagem['text'] = 'Carregue as planilhas primeiro!'
+        return
+    
+    mostraPlanilha(nomesDuplicados[['NOME','INICIO_LOTACAO','NOMESETOR','ORGAO_ENTIDADE']], 'Nomes Duplicados')
+    mostraPlanilha(datasDuplicadas[['NOME', 'INICIO_LOTACAO', 'NOMESETOR', 'ORGAO_ENTIDADE']], 'Datas Duplicadas')
+    mostraPlanilha(planilhasMescladas.loc[planilhasMescladas['IGUAIS'] == False, ['NOME', 'ORGAO_ENTIDADE_MINIBIO', 'ORGAO_ENTIDADE_MENSAL', 'SIGLA', 'IGUAIS']], 'Valores Diferentes')
+
+
+janela = Tk()
+janela.title('Leitor de Planilhas')
+janela.geometry('550x400')
+janela.resizable(False, False)
+janela.option_add('*Font', ('Arial', 8))
+janela.option_add('*Foreground', 'black')
+janela.option_add('*Background', 'white')
+janela.option_add('*Bd', 1)
+janela.option_add('*Relief', 'solid')
+janela.option_add('*Width', 20)
+janela.option_add('*Acivebackground', 'black')
+janela.option_add('*Activeforeground', 'white')
+
+geometria = {'padx': 5, 'pady': 5, 'sticky': 'n'}
+
+labelTitulo = Label(janela, text='Processador de Planilhas')
+labelTitulo.config(bg=labelTitulo.master.cget('bg'), bd=0, font=15, relief='flat', width=30)
+labelTitulo.pack(pady=15)
+
+infos = """
+    Este programa realiza:
+    1. Elimina nomes da planilha mensal que não estejam na planilha minibio
+    2. Elimina da planilha mensal registros antigos de uma mesma pessoa
+    3. Exibe registros de ambas planilhas que estejam com valores diferentes para a coluna ORGAO_ENTIDADE
+    4. Salva em uma nova planilha as alterações realizadas
+    É necessário inserir as duas planilhas para que o processamento ocorra
+"""
+
+labelInfo = Label(janela, text=infos)
+labelInfo.config(bg=labelInfo.master.cget('bg'), bd=0, justify='left', relief='flat', width=0)
+labelInfo.pack(pady=15)
+
+buttonMensal = Button(janela, text='Carregar Mensal', command=lambda: carregaPlanilhas('mensal'))
+buttonMensal.pack(pady=5)
+
+buttonMinibio = Button(janela, text='Carregar Minibio', command=lambda: carregaPlanilhas('minibio'))
+buttonMinibio.pack(pady=5)
+
+buttonProcessa = Button(janela, text='Processar Planilhas', command=lambda: processaPlanilhas() if (Planilhas.planilhaMensal is not None and Planilhas.planilhaMinibio is not None) else labelMensagem.config(text='Carregue as duas planilhas primeiro!'))
+buttonProcessa.pack(pady=5)
+
+labelMensagem = Label(janela, text='')
+labelMensagem.config(bg=labelMensagem.master.cget('bg'), bd=0, relief='flat', width=30)
+labelMensagem.pack(pady=20)
+
+janela.mainloop()
---
+++ Planilhas.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import unicodedata as uni
+
+planilhaMensal = None
+planilhaMinibio = None
+
+def normalizaTexto(texto):
+    texto = str(texto).upper()
+    texto = ''.join(c for c in uni.normalize('NFD', texto) if uni.category(c) != 'Mn')
+    texto = texto.replace('-', '').replace('.', '')
+
+    return texto
+
+def criaSigla(palavra, sigla):
+    global planilhaMensal
+
+    sigla = normalizaTexto(sigla)
+
+    if planilhaMensal is not None:
+        planilhaMensal.loc[planilhaMensal['ORGAO_ENTIDADE'].str.contains(palavra, case=False, na=False), 'SIGLA'] = sigla
+
+def defineSiglas():
+  #Cria coluna com as siglas dos órgãos na planilhaMensal
+  #gabinetes
+  criaSigla('Gabinete do Prefeito', 'GBP')
+  criaSigla('Gabinete do Vice-Prefeito', 'GVP')
+
+  #secretarias
+  criaSigla('Casa Civil', 'CVL')
+  criaSigla('Governo', 'SMG')
+  criaSigla('Coordenação Governamental', 'SMCG')
+  criaSigla('Fazenda', 'SMF')
+  criaSigla('Integridade e Transparência', 'SMIT')
+  criaSigla('Desenvolvimento Urbano', 'SMDU')
+  criaSigla('Desenvolvimento Econômico', 'SMDE')
+  criaSigla('Infraestrutura', 'SMI')
+  criaSigla('Transportes', 'SMTR')
+  criaSigla('Conservação', 'SECONSERVA')
+  criaSigla('Educação', 'SME')
+  criaSigla('Assistência Social', 'SMAS')
+  criaSigla('Saúde', 'SMS')
+  criaSigla('Administração', 'SMA')
+  criaSigla('Trabalho e Renda', 'SMTE')
+  criaSigla('Cultura', 'SMC')
+  criaSigla('Pessoa com Deficiência', 'SMPD')
+  criaSigla('Meio Ambiente e Clima', 'SMAC')
+  criaSigla('Esportes', 'SMEL')
+  criaSigla('Habitação', 'SMH')
+  criaSigla('Ciência, Tecnologia e Inovação', 'SMCT')
+  criaSigla('Envelhecimento Saudável e Qualidade de Vida', 'SEMESQV')
+  criaSigla('Ordem Pública', 'SEOP')
+  criaSigla('Proteção e Defesa dos Animais', 'SMPDA')
+  criaSigla('Turismo', 'SMTUR-RIO')
+  criaSigla('Proteção e Defesa do Consumidor', 'SEDECON')
+  criaSigla('Políticas para Mulheres e Cuidados', 'SPM-RIO')
+  criaSigla('Juventude Carioca', 'JUV-RIO')
+  criaSigla('Ação Comunitária', 'SEAC-RIO')
+  criaSigla('Cidadania e Família', 'SECID')
+  criaSigla('Integração Metropolitana', 'SEIM')
+  criaSigla('Economia Solidária', 'SES-RIO')
+  criaSigla('Inclusão', 'SEI-RIO')
+  criaSigla('Direitos Humanos e Igualdade Racial', 'SEDHIR')
+  criaSigla('Controladoria Geral', 'CGM-Rio')
+  criaSigla('Procuradoria Geral', 'PGM')
+
+  #institutos
+  criaSigla('Previdência e Assistência', 'PREVI-RIO')
+  criaSigla('Urbanismo Pereira Passos', 'IPP')
+  criaSigla('Guarda Municipal', 'GM-RIO')
+
+  #fundações
+  criaSigla('Geotécnica', 'GEO-RIO')
+  criaSigla('Águas do Município', 'RIO-ÁGUAS')
+  criaSigla('Parques e Jardins', 'FPJ')
+  criaSigla('Planetário', 'PLANETÁRIO')
+  criaSigla('Jardim Zoológico', 'RIO-ZOO')
+  criaSigla('Cidade das Artes', 'CIDADE DAS ARTES')
+
+  #empresas
+  criaSigla('Multimeios', 'MULTIRIO')
+  criaSigla('Distribuidora de Filmes', 'RIOFILME')
+  criaSigla('Informática', 'IPLANRIO')
+  criaSigla('Artes Gráficas', 'IMPRENSA DA CIDADE')
+  criaSigla('Parcerias e Investimentos', 'CCPAR')
+  criaSigla('Urbanização', 'RIO-URBE')
+  criaSigla('Turismo do Município', 'RIOTUR')
+  criaSigla('Pública de Saúde', 'RIOSAÚDE')
+  criaSigla('Energia e Iluminação', 'RIOLUZ')
+  criaSigla('Limpeza Urbana', 'COMLURB')
+  criaSigla('Engenharia de Tráfego', 'CET-RIO')
+  criaSigla('Transportes Coletivos', 'CMTC Rio')
+  criaSigla('Feiras, Exposições e Congressos', 'RIOCENTRO')
+  criaSigla('Fomento do Município', 'INVEST.RIO')
+
+def processaPlanilhas():
+    global planilhaMensal, planilhaMinibio
+
+    if planilhaMensal is None or planilhaMinibio is None:
+        return None, None
+
+    #Configurações iniciais
+    pd.set_option('display.max_rows', None)
+    planilhaMensal['INICIO_LOTACAO'] = planilhaMensal['INICIO_LOTACAO'].dt.strftime('%d/%m/%Y')
+    planilhaMinibio['ORGAO_ENTIDADE'] = planilhaMinibio['ORGAO_ENTIDADE'].apply(normalizaTexto)
+
+    #Elimina nomes que não estão na minibio
+    planilhaMensal = planilhaMensal[planilhaMensal['NOME'].isin(planilhaMinibio['NOME'])]
+
+    #Salva valores duplicados
+    nomesDuplicados = planilhaMensal[planilhaMensal.duplicated(subset=['NOME'], keep=False)]
+    datasDuplicadas = planilhaMensal[planilhaMensal.duplicated(subset=['NOME', 'INICIO_LOTACAO'], keep=False)]
+    registroAntigo = nomesDuplicados.loc[nomesDuplicados.groupby('NOME')['INICIO_LOTACAO'].idxmin()]
+
+    planilhaMensal.drop(registroAntigo.loc[~registroAntigo.index.isin(datasDuplicadas.index)].index, inplace=True)
+
+    defineSiglas()
+
+    #Junta as duas planilhas, compara se o valor da coluna ORGAO_ENTIDADE é igual ao da coluna SIGLA e cria um txt com os que forem diferentes
+    planilhasMescladas = planilhaMensal.merge(planilhaMinibio, on = 'NOME', how = 'inner', suffixes = ('_MENSAL', '_MINIBIO'))
+    planilhasMescladas['IGUAIS'] = planilhasMescladas['ORGAO_ENTIDADE_MINIBIO'] == planilhasMescladas['SIGLA']
+
+    #with open('Registros com valores diferentes para ORGAO_ENTIDADE.txt', 'a', encoding='utf-8') as arquivo:
+        #for i in range(len(planilhasMescladas)):
+            #if planilhasMescladas.loc[i, 'IGUAIS'] == False:
+                #registro = planilhasMescladas.loc[i, ['NOME', 'ORGAO_ENTIDADE_MINIBIO', 'SIGLA', 'IGUAIS']]
+                #arquivo.write(registro)
+
+    #Salva planilha mensal com as alterações realizadas
+    #planilhaMensal.to_excel('Planilha Mensal - ALTERADA.xlsx')
+
+    return nomesDuplicados, datasDuplicadas, planilhasMescladas
